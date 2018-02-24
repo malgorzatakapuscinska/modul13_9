a60635e716332ff357c41dc6b6492a1508cd4a55
Malgorzata Kapuscinska <m_kapuscinska1@o2.pl>
2018-02-24 21:45:37 +0100

    Add save fileUrl i n file_url.txt

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,29 +1,33 @@
-var fs = require('fs');
+﻿var fs = require('fs');
 var formidable = require('formidable');
 var mv = require('mv');
 
 exports.upload = function(request, response) {
 	console.log("Rozpoczynam obsługę żądania upload.");
+	console.log(request);
 	var form = new formidable.IncomingForm();
 	form.uploadDir = "./uploaded_files";
-	form.multiples = true;
+	/*form.multiples = true;*/
 	form.keepExtensions = true;
-	form.parse(request, function(error, fields, files) {
-		/*console.log(files.upload.path);*/
-		/*mv(files.upload.path, '.\/test.jpg', function(err)
-				{if(err) throw err;
-				console.log('Files moved succesfully');
-				});*/
-		var fileName = files.upload;
-		var fileName = fileName.name
-		/*var fileName = fileName.name;*/
-		console.log(fileName);
+	form.parse(request, function(error, fields, files){
+	
+		var urlFile = "uploaded_files/" + files.upload.name;
+		var urlFile = urlFile.toString();
+		mv(files.upload.path, urlFile, function(err)
+		{if(err) throw error;
+		console.log('Files moved succesfully');
+		});
+		/*fs.renameSync(files.upload.path, urlFile);*/
 		fs.readFile('templates/upload.html',function(err, html){
 			response.writeHead('200', {'Content-Type': "text/html; charset=utf-8"});
 			response.write(html);
 			response.write("received image:<br/>");
 			response.write("<img src='/show' />");
 			response.end();
+		});
+		fs.writeFileSync('url_file.txt', urlFile, 'utf-8', function(err){
+			if(err){ throw err;
+			} else {console.log('url_file.txt written');}
 		});
 		
 	});
@@ -47,7 +51,10 @@ exports.error = function(request, response) {
 }
 
 exports.show = function(request, response) {
-	fs.readFile("test.jpg", "binary", function(error, file) {
+	var urlFile = fs.readFileSync('url_file.txt', 'utf-8',  function(err){
+		if(err) throw err;
+	});
+	fs.readFile(urlFile, "binary", function(error, file) {
 		response.writeHead(200, {"Content-Type": "image/jpg"});
 		response.write(file, "binary");
 		response.end();
